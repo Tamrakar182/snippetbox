0fe8d76454b037ca396437299752f05787953971
Tamrakar182 <manjultamrakar4@gmail.com>
2025-06-08 22:26:31 +0545

    completed to ch 8

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"snippetbox/internal/models"
 
+	"github.com/go-playground/form/v4"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -17,6 +18,7 @@ type application struct {
 	infoLog       *log.Logger
 	snippets      *models.SnippetModel
 	templateCache map[string]*template.Template
+	formDecorder  *form.Decoder
 }
 
 func main() {
@@ -40,11 +42,14 @@ func main() {
 		errorLog.Fatal(err)
 	}
 
+	formDecoder := form.NewDecoder()
+
 	app := &application{
 		errorLog:      errorLog,
 		infoLog:       infoLog,
 		snippets:      &models.SnippetModel{DB: db},
 		templateCache: templateCache,
+		formDecorder:  formDecoder,
 	}
 
 	srv := &http.Server{
