870f209ecb07553c43c4470cf07d1322881e7417
Tamrakar182 <manjultamrakar4@gmail.com>
2025-06-09 21:48:33 +0545

    completed to ch 9

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,17 +8,21 @@ import (
 	"net/http"
 	"os"
 	"snippetbox/internal/models"
+	"time"
 
+	"github.com/alexedwards/scs/mysqlstore"
+	"github.com/alexedwards/scs/v2"
 	"github.com/go-playground/form/v4"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
-	errorLog      *log.Logger
-	infoLog       *log.Logger
-	snippets      *models.SnippetModel
-	templateCache map[string]*template.Template
-	formDecorder  *form.Decoder
+	errorLog       *log.Logger
+	infoLog        *log.Logger
+	snippets       *models.SnippetModel
+	templateCache  map[string]*template.Template
+	formDecorder   *form.Decoder
+	sessionManager *scs.SessionManager
 }
 
 func main() {
@@ -44,12 +48,17 @@ func main() {
 
 	formDecoder := form.NewDecoder()
 
+	sessionManager := scs.New()
+	sessionManager.Store = mysqlstore.New(db)
+	sessionManager.Lifetime = 12 * time.Hour
+
 	app := &application{
 		errorLog:      errorLog,
 		infoLog:       infoLog,
 		snippets:      &models.SnippetModel{DB: db},
 		templateCache: templateCache,
 		formDecorder:  formDecoder,
+		sessionManager: sessionManager,
 	}
 
 	srv := &http.Server{
